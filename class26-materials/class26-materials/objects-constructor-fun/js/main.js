37155c10535e7fb1ea42b37a3c424dd6c0409804
Armyant98 <Armyant98@users.noreply.github.com>
2023-04-09 21:53:29 -0400

    constructor added to C26

--- class26-materials/class26-materials/objects-constructor-fun/js/main.js
+++ class26-materials/class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,21 @@
 //Create a constructor with 4 properties and 3 methods
+function MakePizza(crust, toppings, shape, sauce ) {
+    this.crust = crust,
+    this.toppings = toppings,
+    this.shape = shape,
+    this.sauce = sauce,
+
+    this.cook = function() {
+        console.log('sizzle sizzle')
+    }
+    
+    this.eat = function (){
+        console.log('nom nom nom')
+    }
+    
+    this.rip = function(){
+        console.log('*rip slices*')
+    }
+
+
+}
--- class26-materials/class26-materials/objects-lost-galaxy/js/main.js
+++ class26-materials/class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,20 @@
-//Create a dog object that has four properties and three methods
+//Create a pizza object that has four properties and three methods
+
+let pizza = {}
+
+pizza.crust = 'stuffed'
+pizza.toppings = ['pepperoni', 'onions', 'red pepper flakes']
+pizza.shape = 'circle'
+
+pizza.cook = function() {
+    console.log('sizzle sizzle')
+}
+
+pizza.eat = function (){
+    console.log('nom nom nom')
+}
+
+pizza.rip = function(){
+    console.log('*rip slices*')
+}
+
