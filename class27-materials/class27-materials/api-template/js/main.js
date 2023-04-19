1a14784cb71ab8094720db93b62439cea4ae24f1
Armyant98 <102646883+Armyant98@users.noreply.github.com>
2023-04-19 16:05:27 -0400

    added card game api work and local storage

--- class27-materials/class27-materials/api-template/js/main.js
+++ class27-materials/class27-materials/api-template/js/main.js
@@ -1,17 +1,29 @@
 //Example fetch using pokemonapi.co
+
+
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  const choice = document.querySelector('input').value.toLowerCase()
+  const url = `https://api.nasa.gov/planetary/apod?api_key=VRaRk17xAawGcmEkzsQE1D4nSBNRPKKJN2ZT6Vhh&date=${choice}`  //date is another key and the ampersand allows for another request within the request
+  console.log(choice)
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        if(data.media_type === 'video'){
+          document.querySelector('iframe').src = data.url
+          document.querySelector('img').src = ''
+
+        }else{
+          document.querySelector('img').src = data.hdurl
+          document.querySelector('iframe').src = ''
+        }
+        
+        document.querySelector('h3').innerText = data.explanation
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
-
