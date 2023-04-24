47c069d037ab4d0986e1845f50e3a28598934ce3
Armyant98 <102646883+Armyant98@users.noreply.github.com>
2023-04-24 16:19:20 -0400

    added dynamic movie addition functionality

--- class27-materials/class27-materials/starWars-api/js/main.js
+++ class27-materials/class27-materials/starWars-api/js/main.js
@@ -5,16 +5,24 @@ function getFetch(){
   
   const choice = Math.floor(Math.random() * 83)
   const url = `https://swapi.dev/api/people/${choice}/`
+  document.getElementById('charFilms').innerHTML = ''
  
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        console.log(data.films)
+        document.getElementById('charName').innerText = (data.name)
         data.films.forEach(film =>{
           fetch(film)
           .then(res => res.json())
-          .then(films => console.log(films.title))
+          .then(films => {
+            const node = document.createElement("li");
+            const textnode = document.createTextNode(films.title)
+            node.appendChild(textnode)
+            console.log(node)
+            document.getElementById('charFilms').appendChild(node)
+            
+          })
           .catch(err => console.log(`error ${err}`))
         })
         fetch(data.homeworld)
